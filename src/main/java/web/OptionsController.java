f2757eef552b3f6b78a909c11cac4d4a9c2f354d
HesusFTW <postelnicu.george@gmail.com>
2016-06-27 20:04:21 +0300

    CHANGE - Added LOGGER.

--- src/main/java/web/OptionsController.java
+++ src/main/java/web/OptionsController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import data.AjaxResponseBody;
 import data.GenericJacksonConverter;
 import data.SearchCriteria;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +17,26 @@ import java.nio.file.Paths;
 @RestController
 public class OptionsController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(OptionsController.class);
+
     @RequestMapping(value = "/getOptions")
     public AjaxResponseBody getSearchResultViaAjax(@RequestBody SearchCriteria search) {
         AjaxResponseBody ajaxResponseBody = new AjaxResponseBody();
         ObjectMapper mapper = new ObjectMapper();
         try {
+            LOGGER.info(String.valueOf(search));
             byte[] environmentsData = Files.readAllBytes(Paths.get("/static/json/envCorrelation.json"));
             byte[] testTypesData = Files.readAllBytes(Paths.get("/static/json/testTypes.json"));
 
             GenericJacksonConverter environments = mapper.readValue(environmentsData, GenericJacksonConverter.class);
             GenericJacksonConverter testTypes = mapper.readValue(testTypesData, GenericJacksonConverter.class);
 
-            System.out.println(environments);
-            System.out.println(testTypes);
+            LOGGER.info(String.valueOf(environments));
+            LOGGER.info(String.valueOf(testTypes));
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            LOGGER.error(e.getMessage());
         }
-        return ajaxResponseBody;
+        return null;
     }
 }
