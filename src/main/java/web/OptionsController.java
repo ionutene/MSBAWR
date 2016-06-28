d909f77e73569780fa767bcab82c757789c8a70c
HesusFTW <postelnicu.george@gmail.com>
2016-06-28 15:11:40 +0300

    CHANGE - Switched to log4j2; * Loaded static json files and sent POST json.

--- src/main/java/web/OptionsController.java
+++ src/main/java/web/OptionsController.java
@@ -1,38 +1,47 @@
 package web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.AjaxResponseBody;
-import data.GenericJacksonConverter;
 import data.SearchCriteria;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class OptionsController {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(OptionsController.class);
+    private static final Logger LOGGER = LogManager.getLogger(OptionsController.class);
+
+    @Autowired
+    ServletContext servletContext;
 
     @RequestMapping(value = "/getOptions")
     public AjaxResponseBody getSearchResultViaAjax(@RequestBody SearchCriteria search) {
         AjaxResponseBody ajaxResponseBody = new AjaxResponseBody();
         ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> data = new HashMap<>();
         try {
-            LOGGER.info(String.valueOf(search));
-            byte[] environmentsData = Files.readAllBytes(Paths.get("/static/json/envCorrelation.json"));
-            byte[] testTypesData = Files.readAllBytes(Paths.get("/static/json/testTypes.json"));
+            LOGGER.info(search);
 
-            GenericJacksonConverter environments = mapper.readValue(environmentsData, GenericJacksonConverter.class);
-            GenericJacksonConverter testTypes = mapper.readValue(testTypesData, GenericJacksonConverter.class);
+            byte[] environmentsData = Files.readAllBytes(Paths.get(servletContext.getRealPath("/static/json/envCorrelation.json")));
+            byte[] testTypesData = Files.readAllBytes(Paths.get(servletContext.getRealPath("/static/json/testTypes.json")));
 
-            LOGGER.info(String.valueOf(environments));
-            LOGGER.info(String.valueOf(testTypes));
+            // convert JSON string to Map
+            data = mapper.readValue(environmentsData, new TypeReference<Map<String, String>>(){});
+            LOGGER.info(data);
+            data = mapper.readValue(testTypesData, new TypeReference<Map<String, String>>(){});
+            LOGGER.info(data);
 
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
